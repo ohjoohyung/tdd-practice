c28392f53b7d483ade78e009b362e318b9f94c4d
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-01-13 23:25:46 +0900

    add: chap02 Calculator Test 완료

---
+++ src/main/java/chap02/Calculator.java
@@ -0,0 +1,9 @@
+package chap02;
+
+public class Calculator {
+    public static int plus(int a1, int a2) {
+        //TDD는 테스트를 먼저 작성하고 테스트에 실패하면 테스트를 통과 시킬 만큼 코드를 추가하는 과정을
+        //반복하면서 점진적으로 기능을 완성해 나간다.
+        return a1+a2; //return 3; 코드를 수정함
+    }
+}
