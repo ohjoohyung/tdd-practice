809ceee7bd990955f174e7bcb325001f06bf0f6c
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-01-18 19:42:26 +0900

    add: chap03 예외 상황 테스트 추가까지 완료

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -1,9 +1,24 @@
 package chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
-    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return billingDate.plusMonths(1);
+    public LocalDate calculateExpiryDate(PayData payData) {
+        int addedMonths = payData.getPayAmount() / 10_000;
+        if (payData.getFirstBillingDate() != null) {
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+            if (payData.getFirstBillingDate().getDayOfMonth() !=
+                    candidateExp.getDayOfMonth()) {
+                if (YearMonth.from(candidateExp).lengthOfMonth() <
+                        payData.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(
+                            YearMonth.from(candidateExp).lengthOfMonth());
+                }
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+            }
+        }
+
+        return payData.getBillingDate().plusMonths(addedMonths);
     }
 }
